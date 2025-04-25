55754c0a8cef1d9935239a44934d7941e24e253d
Joao Geraldo <117011183+JoaoGeraldoS@users.noreply.github.com>
2025-04-25 10:31:24 -0300

    Update Main.java

--- Atividade-mercado/src/Main.java
+++ Atividade-mercado/src/Main.java
@@ -5,19 +5,8 @@ import conexao.ConexaoBD;
 public class Main {
     public static void main(String[] args) {
 
-        ConexaoBD conexaoBD = new ConexaoBD();
-
-        conexaoBD.conexao();
-
-        Tabelas tabelas = new Tabelas();
-//        tabelas.criaTabelas();
-
         MenuPrincipal menuPrincipal= new MenuPrincipal();
         menuPrincipal.menuPrincipal();
-
-
-
-
 
     }
 }
